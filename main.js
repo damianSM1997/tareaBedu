e857e9afb7ec7c96ab28d3e1bf5ef7d4cad1ee11
damianSM1997 <damiansm97@gmail.com>
2021-01-19 21:39:46 -0600

    Se agregan comentarios para entender el funcionamiento del codigo

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 
 /**
+ * Damian Susano Martinez
+ * Jorge Ivan Gutierrez Yañez
+ * Diana Yesenia Campos Tenorio
+ * Daniel Rodríguez López
+ * Anahi Camas Hernández
+ */
+
+/**
  * deepEqual
  */
 function deepEqual(a,b) {  
+  //se compara la longitud a y b y si es distinto retorna false
    if(Object.keys(a).length !== Object.keys(b).length){ 
      return false;
    }
+   //checa si a y b son objetos
    if(typeof(a) === typeof(b)){
+    //se hace un ciclo para poder interar en los balores de a o en en la posision key1
    for( const key1 in a){  
         if(a[key1] !== b[key1]){
           return false;
         }
      }
    }
-
+   // si pasa los dos filtros quiere desir que son iguales
    return true;
  }
 
@@ -32,12 +43,27 @@ console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
  * 
  * chuck
  */
-
 function chunck(array, size){
+  // se crea una variable tipo objeto para cuardar el resultado
   var res = [];
+  /**
+   * el ciclo dice que mientras i sea menor a la longitud del areglo entonces seguira,
+   *  "i" aumentara su valor actual mas lo grande que desea ser el areglo
+   * */ 
   for (let i = 0; i < array.length; i += size) {
+    /**
+     * se agrega a el areglo el valor que hay en el areglo original("array") de dicho areglo se
+     * le aplica la funcion slice que genera un nuevo areglo sin afectar el areglo original
+     * slice toma dos parametros, (inicio, fin) entonces toma el valor que contenga i en ese momento
+     * como inicio y el final como el valor que tenga i en ese momento + lo grande del areglo
+     * 
+     * "i += size" se aplica porque asi la siguiente interacion no sera el valor de i + 1
+     * la nueva interacion es definida entonces por elvalor de lo grande que quieras que sea
+     * cada areglo determinada por el parametro "size"
+     */
     res.push(array.slice(i, i + size));    
   }
+  //retorna como respuesta lo que tenga dentro del arrar res
   return res;
 }
 
@@ -47,6 +73,9 @@ console.log('Test 2: ' , chunck(data, 2));
 console.log('Test 3: ' , chunck(data, 3));
 
 
+/**
+ * frequency
+ */
 function frequency(string) {
   var obj ={};
   var caracter;
